54c9e09af70d73af56dd79827701a632ac02f086
rafaScalet <rafaelscalet2018@gmail.com>
2024-09-27 16:46:45 -0300

    first commit

---
+++ src/main/java/com/music/music_registration/MusicRegistrationApplication.java
@@ -0,0 +1,13 @@
+package com.music.music_registration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicRegistrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/music/music_registration/MusicRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.music.music_registration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
